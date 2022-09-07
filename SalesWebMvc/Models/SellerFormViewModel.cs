cd0c461f2885d1d72f63f066d8b69a4444a32dc2
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-09-07 00:30:54 -0300

    sellerFormViewModel and Department select component

---
+++ SalesWebMvc/Models/SellerFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models
+{
+    public class SellerFormViewModel
+    {
+        public Seller seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
