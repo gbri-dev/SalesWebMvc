d8f55569b2f752566560610009ece53c121140cb
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-09-17 17:50:41 -0300

    Update seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
+
 namespace SalesWebMvc.Services
 {
     public class SellerService
     {
         private readonly SalesWebMvcContext _context;
-            public SellerService(SalesWebMvcContext context)
+        public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
@@ -32,6 +34,14 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
         }
     }
 }
