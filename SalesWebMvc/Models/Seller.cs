f677bf3ed1937080b45e315686a200cdc7289daf
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-09-20 21:27:21 -0300

    app locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -15,9 +15,11 @@ namespace SalesWebMvc.Models
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "${0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
